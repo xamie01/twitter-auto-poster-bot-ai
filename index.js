6321e303b8bca86f3a5a2656748d0d035b3f2c87
xamie01 <69655889+xamie01@users.noreply.github.com>
2025-06-01 22:19:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   // Write your prompt here
   const prompt =
-    "generate a web development content, tips and tricks or something new or some rant or some advice as a tweet, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis";
+    " Research this  set of cryptocurrency projects Union Build, LayerEdge, and Caldera randomly select one, and generate a Twitter post under 280 characters but at least 200 characters  that includes: a brief up to date information of the project, a relevant market statistic or funding detail, , the hashtags #Crypto, #Blockchain, and check X post to see orther relevant hahtags and reserch engaging question to spark discussion. The post must be educational, avoid promotional language or financial advice, and comply with Twitter's content guidelines, ensuring no misleading claims are made.";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
